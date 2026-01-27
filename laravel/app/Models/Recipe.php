71020a35273eb8f24c5b361be5d08de9e0139f8c
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-27 16:30:12 +0100

    added relationships in recipe model

--- laravel/app/Models/Recipe.php
+++ laravel/app/Models/Recipe.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    protected $fillable = ['recipeTitle', 'recipeDescription', 'image'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
+
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
 }
