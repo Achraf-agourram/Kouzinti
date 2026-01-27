6b6baa39d69a8e3491c7c7ceb32a979058357fd0
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-27 15:18:27 +0100

    added comments table with their properties

--- laravel/database/migrations/2026_01_27_093006_create_comments_table.php
+++ laravel/database/migrations/2026_01_27_093006_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('commentContent');
+            $table->foreignId('recipe_id')->constrained()->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
