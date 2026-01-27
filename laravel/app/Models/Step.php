6cb436b6b4f0adc24d1f370fdbca3c53c5286f19
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-27 16:30:33 +0100

    added relationships in step model

--- laravel/app/Models/Step.php
+++ laravel/app/Models/Step.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Step extends Model
 {
-    //
+    protected $fillable = ['stepDescription', 'stepOrder'];
+
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class);
+    }
 }
