e11b5a9fb32ce4aa218a7293c195f30692bd7cce
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-27 16:29:33 +0100

    added relationships in comment model

--- laravel/app/Models/Comment.php
+++ laravel/app/Models/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['commentContent'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class);
+    }
 }
