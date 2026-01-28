de2a048388136adbbc642e5bc38f13bbfd4abdcf
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-28 10:14:53 +0100

    added home route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'authPage']);
@@ -8,3 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/register', [AuthController::class, 'authPage']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/home', [RecipeController::class, 'recipesPage']);
