5d490bd1877cb2c11da1b604944e6e2ebbfeb222
Achraf-podcast <achrafagourram6@gmail.com>
2026-01-28 10:13:29 +0100

    fix: added return to redirect

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
         if (Auth::attempt($request->only(['email', 'password']))) {
             $request->session()->regenerate();
 
-            redirect('/home');
+            return redirect('/home');
         }
 
         return redirect('/login')->with('failed', 'Invalid credentials');
